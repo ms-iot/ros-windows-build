c29386ab316b93b854cf46ac6ac6a4ab44bcf705
seanyen <seanyen@corp.microsoft.com>
2019-07-16 11:25:56 -0700

    Enumerate the objects at the correct loop.

--- docker/garbage-collection.ps1
+++ docker/garbage-collection.ps1
@@ -1,11 +1,13 @@
 $ACR_NAME = $env:CONTAINER_REGISTRY
 az acr repository list -n $ACR_NAME | ConvertFrom-Json | ForEach-Object {
-    $repository = $_
-    az acr repository show-manifests -n $ACR_NAME --repository $repository | ConvertFrom-Json | ForEach-Object {
-        $_ | ForEach-Object {
-            $digest = $_.digest
-            if(!$_.tags) {
-                az acr repository delete --name $ACR_NAME --image $repository@$digest --yes
+    $_ | ForEach-Object {
+        $repository = $_
+        az acr repository show-manifests -n $ACR_NAME --repository $repository | ConvertFrom-Json | ForEach-Object {
+            $_ | ForEach-Object {
+                $digest = $_.digest
+                if(!$_.tags) {
+                    az acr repository delete --name $ACR_NAME --image $repository@$digest --yes
+                }
             }
         }
     }
