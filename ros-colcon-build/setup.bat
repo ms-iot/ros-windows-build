14a123563faccac3bfbad52649d76d7f350272c3
seanyen <seanyen@microsoft.com>
2020-01-23 15:06:52 -0800

    [colcon-build] Run test and collect results.

--- ros-colcon-build/setup.bat
+++ ros-colcon-build/setup.bat
@@ -1,4 +1,8 @@
 @echo off
 set PYTHONHOME=C:\opt\python37amd64\
-set PATH=C:\opt\python37amd64\;C:\opt\python37amd64\Scripts;C:\opt\python37amd64\bin;C:\opt\rosdeps\x64\bin;%PATH%
+set PATH=C:\opt\rosdeps\x64\bin;%PATH%
+set PATH=C:\opt\python37amd64\bin;%PATH%
+set PATH=C:\opt\python37amd64\Scripts;%PATH%
+set PATH=C:\opt\python37amd64;%PATH%
+set "PATH=C:\Program Files\Cppcheck;%PATH%"
 set PYTHONPATH=
