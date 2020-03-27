9ddbbd2aa1b6b99b550e28c64929de2bd2cea695
seanyen <seanyen@microsoft.com>
2020-03-27 11:17:51 -0700

    add error check.

--- package-build/tools/rosdepInstall.bat
+++ package-build/tools/rosdepInstall.bat
@@ -5,6 +5,27 @@ setlocal
 pushd %~dp0
 call "setup.bat"
 
+:: check all required environments are set
+if not defined ROS_DISTRO (
+    echo ROS_DISTRO should be defined. 1>&2
+    exit 1
+)
+
+if not defined ROS_ETC_DIR (
+    echo ROS_ETC_DIR should be defined. 1>&2
+    exit 1
+)
+
+if not defined PYTHONHOME (
+    echo PYTHONHOME should be defined. 1>&2
+    exit 1
+)
+
+if not defined ROS_PYTHON_VERSION (
+    echo ROS_PYTHON_VERSION should be defined. 1>&2
+    exit 1
+)
+
 :: use Python from ROS installation
 set PATH=%PYTHONHOME%;%PYTHONHOME%\Scripts;%PATH%
 set PYTHONPATH=
@@ -13,3 +34,4 @@ set PYTHONPATH=
 rosdep init
 rosdep update
 rosdep install --from-paths c:\opt\ros\%ROS_DISTRO%\x64\share --ignore-src -r -y
+if errorlevel 1 exit 1
