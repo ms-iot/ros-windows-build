642b2b0191ee5d1fb6b4f44181f140a2434d9d58
seanyen <seanyen@microsoft.com>
2020-04-08 21:48:15 -0700

    lock the cmake. There is a seems-to-be-broken 3.17 just released hours ago..

--- ros-catkin-build/rosdep_install.bat
+++ ros-catkin-build/rosdep_install.bat
@@ -1,4 +1,5 @@
 @echo off
 :: ignore the errors from rosdep install
 rosdep install --from-paths c:\catkin_ws\src --ignore-src --rosdistro %ROS_DISTRO% -r -y
+pip install --upgrade --force-reinstall cmake==3.16.3
 exit /b 0
