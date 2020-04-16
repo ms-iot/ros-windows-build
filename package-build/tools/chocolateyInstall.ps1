51f0a4d1f4b43497f2d81e7af83f50a20e81e788
Lou Amadio <lamadio@microsoft.com>
2020-04-16 16:12:44 -0700

    Migrate

--- package-build/tools/chocolateyInstall.ps1
+++ package-build/tools/chocolateyInstall.ps1
@@ -11,6 +11,25 @@ $packageArgs = @{
   fileFullPath64  = $fileLocation
 }
 
+$migrationArray = @( 
+  "rqt_moveit-0.5.7-py2.7.egg-info",
+  "rqt_robot_monitor-0.5.9-py2.7.egg-info",
+  "rqt_rviz-0.6.0-py2.7.egg-info"
+)
+
+# As part of a noetic migration, these have been changed to directories. 
+# During unpacking, chocolatey will see this as an error.
+
+# delete these files from old deployment, so the new deployment unpacks correctly
+$migrationPackagesPath = Join-Path $destination 'melodic\x64\lib\site-packages' 
+
+foreach ($migrationFile in $migrationArray) {
+  $migration = Join-Path $migrationPackagesPath $migrationFile
+  if (Test-Path -Path $migration -PathType leaf) {
+    Remove-Item $migration
+  }
+}
+
 # - https://chocolatey.org/docs/helpers-get-chocolatey-unzip
 Get-ChocolateyUnzip @packageArgs
 
