4ef33eda4de5b209e3661d9dd32e7c26e2869560
Sean Yen <seanyen@microsoft.com>
2020-05-09 10:17:03 -0700

    Fold the vcpkg install into the master package for easier diagnostics.

---
+++ package-build/tools/vcpkgInstall.ps1
@@ -0,0 +1,22 @@
+$ErrorActionPreference = 'Stop';
+
+$Uri = 'https://github.com/Microsoft/vcpkg.git'
+$RootDir = 'C:\opt'
+$InstallDir = "$RootDir\vcpkg"
+
+New-Item -ItemType Directory -Path $RootDir -Force | Out-Null
+
+Write-Host 'Deploying vcpkg...'
+if (-Not (Test-Path -Path "$InstallDir\vcpkg.exe" -PathType Leaf)) {
+  if (-Not (Test-Path -Path $InstallDir -PathType Container)) {
+    git clone --depth=1 $Uri $InstallDir -q
+    Invoke-Expression "$InstallDir\bootstrap-vcpkg.bat"
+  } else {
+    throw "remove $InstallDir and reinstall again."
+  }
+}
+
+Write-Host 'Validating vcpkg...'
+Invoke-Expression "$InstallDir\vcpkg.exe version"
+
+Write-Host 'Deploying vcpkg... done.'
