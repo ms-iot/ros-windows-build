ac27f6fcf0b95ee8ce43067688421c49dfdf4f7b
seanyen <seanyen@microsoft.com>
2020-05-22 10:56:03 -0700

    lock the version to the latest release.

--- package-build/tools/vcpkgInstall.ps1
+++ package-build/tools/vcpkgInstall.ps1
@@ -3,13 +3,14 @@ $ErrorActionPreference = 'Stop';
 $Uri = 'https://github.com/Microsoft/vcpkg.git'
 $RootDir = 'C:\opt'
 $InstallDir = "$RootDir\vcpkg"
+$VcpkgVersion = "2020.04"
 
 New-Item -ItemType Directory -Path $RootDir -Force | Out-Null
 
 Write-Host 'Deploying vcpkg...'
 if (-Not (Test-Path -Path "$InstallDir\vcpkg.exe" -PathType Leaf)) {
   if (-Not (Test-Path -Path $InstallDir -PathType Container)) {
-    git clone --depth=1 $Uri $InstallDir -q
+    git clone --depth=1 $Uri $InstallDir -q -b $VcpkgVersion
     Invoke-Expression "$InstallDir\bootstrap-vcpkg.bat"
   } else {
     throw "remove $InstallDir and reinstall again."
