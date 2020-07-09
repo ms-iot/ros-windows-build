bb75959e0dd594f4a3c7a1a63576b00f6fd7bd4e
Sean Yen <seanyen@microsoft.com>
2020-07-09 12:57:37 -0700

    [common] Limit to use 2 cores for builds (#113)

--- ros-colcon-build/test.bat
+++ ros-colcon-build/test.bat
@@ -1,5 +1,6 @@
 @echo off
 pushd c:\colcon_ws
+set CMAKE_BUILD_PARALLEL_LEVEL=2
 set CMAKE_PREFIX_PATH=C:/opt/rosdeps/x64
 set "ROSWIN_COLCON_PACKAGE_UP_TO=--packages-up-to %ROSWIN_METAPACKAGE% %ROSWIN_ADDITIONAL_PACKAGE%"
 if "%ROSWIN_METAPACKAGE%"=="ALL" (
