b811cb956af626a7373d9ab7fc2ba4df4706cc21
seanyen <seanyen@microsoft.com>
2020-08-17 12:08:03 -0700

    Adding.

---
+++ experimental/ros/build_foxy.bat
@@ -0,0 +1,10 @@
+@echo OFF
+
+colcon build ^
+    --continue-on-error ^
+    --merge-install ^
+    --install-base %INSTALL_DIR% ^
+    --cmake-args ^
+        -DCURL_NO_CURL_CMAKE=ON ^
+        -DBUILD_TESTING:BOOL=False ^
+        -DCMAKE_PROGRAM_PATH=%INSTALL_DIR%\tools\protobuf;%INSTALL_DIR%\tools\qt5\bin
