6d0072ecedb74f11bd06bc5abe3dc0a155326e00
seanyen <seanyen@microsoft.com>
2020-08-17 13:01:42 -0700

    Adding.

--- experimental/ros/build_noetic.bat
+++ experimental/ros/build_noetic.bat
@@ -6,11 +6,11 @@ colcon build ^
     --packages-up-to desktop_full ^
     --packages-skip-by-dep stage stage_ros image_view theora_image_transport rviz_plugin_tutorials ^
     --packages-skip stage stage_ros image_view theora_image_transport rviz_plugin_tutorials ^
-    --install-base C:\opt\ros\foxy\x64 ^
+    --install-base %INSTALL_DIR% ^
     --cmake-args ^
         -G Ninja ^
         -DCMAKE_BUILD_TYPE=Release ^
         -DCURL_NO_CURL_CMAKE=ON ^
         -DCATKIN_SKIP_TESTING=ON ^
         -DBUILD_TESTING:BOOL=False ^
-        -DCMAKE_PROGRAM_PATH=c:\opt\ros\foxy\x64\tools\protobuf;c:\opt\ros\foxy\x64\tools\qt5\bin
+        -DCMAKE_PROGRAM_PATH=%INSTALL_DIR%\tools\protobuf;%INSTALL_DIR%\tools\qt5\bin
