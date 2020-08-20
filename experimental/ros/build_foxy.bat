708946b3ac9862af9b9fe84c94fd8cd726e1a174
seanyen <seanyen@microsoft.com>
2020-08-19 17:15:15 -0700

    Fix.

--- experimental/ros/build_foxy.bat
+++ experimental/ros/build_foxy.bat
@@ -5,6 +5,8 @@ set ROS_VERSION=2
 colcon build ^
     --continue-on-error ^
     --merge-install ^
+    --packages-skip-by-dep rttest test_osrf_testing_tools_cpp tlsf ^
+    --packages-skip rttest test_osrf_testing_tools_cpp tlsf ^
     --install-base %INSTALL_DIR% ^
     --cmake-target install ^
     --cmake-args ^
