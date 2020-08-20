3cf2eab8c532f52ff3de5c6b4224b01255c7f483
seanyen <seanyen@microsoft.com>
2020-08-19 22:38:11 -0700

    Adding symbols.

--- experimental/ros/build_foxy.bat
+++ experimental/ros/build_foxy.bat
@@ -10,6 +10,8 @@ colcon build ^
     --install-base %INSTALL_DIR% ^
     --cmake-target install ^
     --cmake-args ^
+        -DCMAKE_BUILD_TYPE=RelWithDebInfo ^
         -DCURL_NO_CURL_CMAKE=ON ^
         -DBUILD_TESTING:BOOL=False ^
-        -DCMAKE_PROGRAM_PATH=%INSTALL_DIR%\tools\protobuf;%INSTALL_DIR%\tools\qt5\bin
+        -DCMAKE_PROGRAM_PATH=%INSTALL_DIR%\tools\protobuf;%INSTALL_DIR%\tools\qt5\bin ^
+        -DCMAKE_PDB_OUTPUT_DIRECTORY=%PDB_OUTPUT_DIRECTORY%
