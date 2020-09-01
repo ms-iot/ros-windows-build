b815c3abf8ca8ac49cba8987ff179eacb47ebadd
seanyen <seanyen@microsoft.com>
2020-08-31 22:30:25 -0700

    Adding.

--- experimental/ros/build_ros1.bat
+++ experimental/ros/build_ros1.bat
@@ -3,8 +3,8 @@
 colcon build ^
     --continue-on-error ^
     --merge-install ^
-    --packages-skip-by-dep stage stage_ros image_view theora_image_transport rviz_plugin_tutorials ^
-    --packages-skip stage stage_ros image_view theora_image_transport rviz_plugin_tutorials ^
+    --packages-skip-by-dep stage stage_ros image_view theora_image_transport rviz_plugin_tutorials four_wheel_steering_controller ^
+    --packages-skip stage stage_ros image_view theora_image_transport rviz_plugin_tutorials four_wheel_steering_controller ^
     --install-base %INSTALL_DIR% ^
     --cmake-args ^
         -G Ninja ^
