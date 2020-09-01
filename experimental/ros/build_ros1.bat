92a0652a414f95c34850838a19bc81ea27d8fb4d
seanyen <seanyen@microsoft.com>
2020-09-01 03:12:19 -0700

    Adding.

--- experimental/ros/build_ros1.bat
+++ experimental/ros/build_ros1.bat
@@ -2,7 +2,6 @@
 
 colcon build ^
     --merge-install ^
-    --parallel-workers 1 ^
     --packages-up-to desktop_full ^
     --packages-skip-by-dep stage stage_ros image_view theora_image_transport rviz_plugin_tutorials four_wheel_steering_controller ^
     --packages-skip stage stage_ros image_view theora_image_transport rviz_plugin_tutorials four_wheel_steering_controller ^
@@ -17,7 +16,6 @@ colcon build ^
 
 colcon build ^
     --merge-install ^
-    --parallel-workers 1 ^
     --packages-up-to moveit ^
     --packages-skip-by-dep stage stage_ros image_view theora_image_transport rviz_plugin_tutorials four_wheel_steering_controller ^
     --packages-skip stage stage_ros image_view theora_image_transport rviz_plugin_tutorials four_wheel_steering_controller ^
@@ -32,7 +30,6 @@ colcon build ^
 
 colcon build ^
     --merge-install ^
-    --parallel-workers 1 ^
     --packages-skip-by-dep stage stage_ros image_view theora_image_transport rviz_plugin_tutorials four_wheel_steering_controller ^
     --packages-skip stage stage_ros image_view theora_image_transport rviz_plugin_tutorials four_wheel_steering_controller ^
     --install-base %INSTALL_DIR% ^
