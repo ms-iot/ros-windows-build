a228119b39fe99611b6acae662e66390ea390820
seanyen <seanyen@microsoft.com>
2020-09-01 02:57:53 -0700

    Adding.

--- experimental/ros/build_ros1.bat
+++ experimental/ros/build_ros1.bat
@@ -3,6 +3,21 @@
 colcon build ^
     --merge-install ^
     --parallel-workers 1 ^
+    --packages-up-to desktop_full ^
+    --packages-skip-by-dep stage stage_ros image_view theora_image_transport rviz_plugin_tutorials four_wheel_steering_controller ^
+    --packages-skip stage stage_ros image_view theora_image_transport rviz_plugin_tutorials four_wheel_steering_controller ^
+    --install-base %INSTALL_DIR% ^
+    --cmake-args ^
+        -G Ninja ^
+        -DCMAKE_BUILD_TYPE=RelWithDebInfo ^
+        -DCURL_NO_CURL_CMAKE=ON ^
+        -DCATKIN_SKIP_TESTING=ON ^
+        -DBUILD_TESTING:BOOL=False ^
+        -DCMAKE_PROGRAM_PATH=%INSTALL_DIR%\tools\protobuf;%INSTALL_DIR%\tools\qt5\bin
+
+colcon build ^
+    --merge-install ^
+    --parallel-workers 1 ^
     --packages-skip-by-dep stage stage_ros image_view theora_image_transport rviz_plugin_tutorials four_wheel_steering_controller ^
     --packages-skip stage stage_ros image_view theora_image_transport rviz_plugin_tutorials four_wheel_steering_controller ^
     --install-base %INSTALL_DIR% ^
