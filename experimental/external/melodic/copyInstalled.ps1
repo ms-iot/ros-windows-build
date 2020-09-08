fe75d1e67128e4c61a38191b452966981f2c8e10
seanyen <seanyen@microsoft.com>
2020-09-08 13:31:27 -0700

    Adding.

---
+++ experimental/external/melodic/copyInstalled.ps1
@@ -0,0 +1,44 @@
+[CmdletBinding()]
+param(
+    $badParam,
+    [Parameter(Mandatory=$True)][string]$InstallDir
+)
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
+# Powershell2-compatible way of forcing named-parameters
+if ($badParam)
+{
+    throw "Only named parameters are allowed"
+}
+
+try
+{
+    $LegacyRosdep = "C:\opt\rosdeps\x64"
+    # Copy Rosdep installed
+    $developmentFiles = @(
+        '*.pdb',
+        '*-debug.cmake'
+    )
+    Get-ChildItem -Path $LegacyRosdep | ForEach-Object {
+        if ("debug" -eq $_) {
+            return
+        }
+        $arguments = @{
+            Path = (Join-Path $LegacyRosdep $_)
+            Recurse = $True
+            Destination = $InstallDir
+            Container = $True
+            Exclude = $developmentFiles
+            Force = $True
+        }
+        Copy-Item @arguments
+    }
+
+    $PythonInstalled = "C:\opt\python27amd64"
+    Copy-Item -Path $PythonInstalled -Destination $InstallDir
+}
+catch
+{
+  Write-Warning "Bootstrapping ROS dependencies failed."
+  throw
+}
