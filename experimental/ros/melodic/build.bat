fd6eb67306c808a4f99cebb6f83de90ed2a591bb
seanyen <seanyen@microsoft.com>
2020-09-10 15:30:43 -0700

    FIX.

--- experimental/ros/melodic/build.bat
+++ experimental/ros/melodic/build.bat
@@ -7,7 +7,7 @@ python src\catkin\bin\catkin_make_isolated.py ^
     --install-space "%INSTALL_DIR%" ^
     --use-ninja ^
     --install ^
-    --ignore-pkg "%IGNORED_PACKAGES%" ^
+    --ignore-pkg %IGNORED_PACKAGES% ^
     -DCMAKE_BUILD_TYPE=RelWithDebInfo ^
     -DCATKIN_SKIP_TESTING=ON ^
     -DCURL_NO_CURL_CMAKE=ON
