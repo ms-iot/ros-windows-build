90e1be7412735ed135422d225d0b2afd451fe8b2
seanyen <seanyen@microsoft.com>
2020-09-15 13:52:35 -0700

    Adding more libs from Vcpkg.

---
+++ experimental/external/melodic/vcpkg.ps1
@@ -0,0 +1,66 @@
+[CmdletBinding()]
+param(
+    $badParam,
+    [Parameter(Mandatory=$True)][string]$InstallDir
+)
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
+# Powershell2-compatible way of forcing named-parameters
+if ($badParam)
+{
+    throw "Only named parameters are allowed"
+}
+
+try
+{
+    $scriptsDir = split-path -parent $script:MyInvocation.MyCommand.Definition
+    $vcpkgDir = (join-Path $scriptsDir "vcpkg")
+    $Env:VCPKG_DEFAULT_TRIPLET = "x64-windows"
+
+    $vcpkgDir | ForEach-Object {
+        Remove-Item $_ -Force -Recurse -ErrorAction SilentlyContinue
+        if (Test-Path $_ -PathType Container) {
+            throw "cannot remove $_"
+        }
+
+        New-Item -Path $_ -ItemType directory -Force | Out-Null
+    }
+
+    # download the Vcpkg
+    $url = "https://github.com/microsoft/vcpkg/archive/2020.07.zip"
+    $vcpkgZip = (Join-Path $vcpkgDir "vcpkg-2020.07.zip")
+    Invoke-WebRequest -Uri $url -OutFile $vcpkgZip
+
+    # install the Vcpkg
+    Expand-Archive -LiteralPath $vcpkgZip -DestinationPath $vcpkgDir
+    & "$vcpkgDir\vcpkg-2020.07\bootstrap-vcpkg.bat"
+
+    # build the required ports
+    $vcpkgAnswerFile = (Join-Path $scriptsDir "vcpkg.in")
+    & "$vcpkgDir\vcpkg-2020.07\vcpkg.exe" install @$vcpkgAnswerFile
+
+    $vcpkgInstalled = "$vcpkgDir\vcpkg-2020.07\installed\x64-windows"
+    $developmentFiles = @(
+        '*.pdb',
+        '*-debug.cmake'
+    )
+    Get-ChildItem -Path $vcpkgInstalled | ForEach-Object {
+        if ("debug" -eq $_) {
+            return
+        }
+        $arguments = @{
+            Path = (Join-Path $vcpkgInstalled $_)
+            Recurse = $True
+            Destination = $InstallDir
+            Container = $True
+            Exclude = $developmentFiles
+            Force = $True
+        }
+        Copy-Item @arguments
+    }
+}
+catch
+{
+  Write-Warning "Bootstrapping ROS dependencies failed."
+  throw
+}
