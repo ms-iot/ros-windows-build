a1a64ecd6bbd0fac67223c8ca84a2207ea93060c
seanyen <seanyen@microsoft.com>
2020-09-17 02:35:31 -0700

    bootstrap rosdep.

--- experimental/ros/melodic/patch.bat
+++ experimental/ros/melodic/patch.bat
@@ -7,6 +7,14 @@ python -m pip install -U vcstool
 python -m pip install -U catkin_pkg
 python -m pip install -U rosinstall_generator
 python -m pip install -U rosinstall
+python -m pip install -U git+https://github.com/ms-iot/rosdep@windows/0.19.0
 
 copy /Y %INSTALL_DIR%\tools\protobuf\protoc.exe %INSTALL_DIR%\bin
 xcopy /Y /S /I %~dp0patch %INSTALL_DIR%
+
+:: bootstrap rosdep
+set ROS_ETC_DIR=%INSTALL_DIR%\etc\ros
+rosdep init
+rosdep update
+rosdep check --from-paths "%WORKSPACE_DIR%\src" --ignore-src 2>&1
+exit 0
