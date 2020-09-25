af3559b69f2094e288d638af78db76e48b1385f5
seanyen <seanyen@microsoft.com>
2020-09-24 21:21:57 -0700

    Ingest Vcpkg tools into install layout.

---
+++ experimental/ros/melodic/patch/etc/catkin/profile.d/999.vcpkg.bat
@@ -0,0 +1,17 @@
+@echo off
+
+:: initialize Vcpkg environments
+set VCPKG_ROOT=c:\opt\ros\melodic\x64\tools\vcpkg
+set VCPKG_DEFAULT_TRIPLET=x64-windows
+
+:: add Vcpkg.exe to PATH
+set PATH=%PATH:C:\opt\ros\melodic\x64\tools\vcpkg;=%
+set "PATH=C:\opt\ros\melodic\x64\tools\vcpkg;%PATH%"
+
+:: add the installed bin to PATH
+set PATH=%PATH:C:\opt\ros\melodic\x64\tools\vcpkg\installed\x64-windows\bin;=%
+set "PATH=C:\opt\ros\melodic\x64\tools\vcpkginstalled\x64-windows\bin;%PATH%"
+
+:: add the installed root for CMake
+set CMAKE_PREFIX_PATH=%CMAKE_PREFIX_PATH:C:\opt\ros\melodic\x64\tools\vcpkg\installed\x64-windows;=%
+set "CMAKE_PREFIX_PATH=C:\opt\ros\melodic\x64\tools\vcpkg\installed\x64-windows;%CMAKE_PREFIX_PATH%"
