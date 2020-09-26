be38e2a3747c128dde399f5e0fe422672b7b8e5e
Sean Yen <seanyen@microsoft.com>
2020-09-26 01:43:51 -0700

    [vnext] [noetic] Adding rosdep integration. (#160)

--- experimental/ros/noetic/build.bat
+++ experimental/ros/noetic/build.bat
@@ -15,3 +15,51 @@ colcon build ^
         -DBUILD_TESTING:BOOL=False ^
         -DCMAKE_PROGRAM_PATH=%INSTALL_DIR%\tools\protobuf;%INSTALL_DIR%\tools\qt5\bin ^
     2>&1
+if errorlevel 1 exit 1
+
+move /Y %INSTALL_DIR%\lib\*.dll %INSTALL_DIR%\bin
+if errorlevel 1 exit 1
+
+move /Y %INSTALL_DIR%\Lib\gazebo_plugins\gazebo_ros_wheel_slip.dll %INSTALL_DIR%\bin
+if errorlevel 1 exit 1
+
+move /Y %INSTALL_DIR%\Lib\laser_filters\laser_scan_filters.dll %INSTALL_DIR%\bin
+if errorlevel 1 exit 1
+
+move /Y %INSTALL_DIR%\Lib\laser_filters\pointcloud_filters.dll %INSTALL_DIR%\bin
+if errorlevel 1 exit 1
+
+move /Y %INSTALL_DIR%\Lib\nodelet_tutorial_math\nodelet_math.dll %INSTALL_DIR%\bin
+if errorlevel 1 exit 1
+
+move /Y %INSTALL_DIR%\Lib\pluginlib_tutorials\pluginlib_tutorials.dll %INSTALL_DIR%\bin
+if errorlevel 1 exit 1
+
+move /Y %INSTALL_DIR%\Lib\filters\increment.dll %INSTALL_DIR%\bin
+if errorlevel 1 exit 1
+
+move /Y %INSTALL_DIR%\Lib\filters\mean.dll %INSTALL_DIR%\bin
+if errorlevel 1 exit 1
+
+move /Y %INSTALL_DIR%\Lib\filters\median.dll %INSTALL_DIR%\bin
+if errorlevel 1 exit 1
+
+move /Y %INSTALL_DIR%\Lib\filters\params.dll %INSTALL_DIR%\bin
+if errorlevel 1 exit 1
+
+move /Y %INSTALL_DIR%\Lib\filters\transfer_function.dll %INSTALL_DIR%\bin
+if errorlevel 1 exit 1
+
+move /Y %INSTALL_DIR%\Lib\rviz_imu_plugin\rviz_imu_plugin.dll %INSTALL_DIR%\bin
+if errorlevel 1 exit 1
+
+move /Y %INSTALL_DIR%\Lib\ackermann_steering_controller\ackermann_steering_controller.dll %INSTALL_DIR%\bin
+if errorlevel 1 exit 1
+
+move /Y %INSTALL_DIR%\Lib\rosparam_shortcuts\rosparam_shortcuts.dll %INSTALL_DIR%\bin
+if errorlevel 1 exit 1
+
+:: run rosdep check
+set ROS_ETC_DIR=%INSTALL_DIR%\etc\ros
+rosdep check --from-paths "%INSTALL_DIR%\share" --ignore-src 2>&1
+exit 0
