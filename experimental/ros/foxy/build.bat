db539ddd37d0ec31601db1bcdeb41665651e3a60
seanyen <seanyen@microsoft.com>
2020-09-30 22:16:46 -0700

    Fix foxy build.

--- experimental/ros/foxy/build.bat
+++ experimental/ros/foxy/build.bat
@@ -16,6 +16,8 @@ colcon build ^
         -DCMAKE_PROGRAM_PATH=%INSTALL_DIR%\tools\protobuf;%INSTALL_DIR%\tools\qt5\bin ^
         -DCMAKE_PDB_OUTPUT_DIRECTORY=%PDB_OUTPUT_DIRECTORY% ^
     2>&1
+if errorlevel 1 exit 1
 
 :: fix srdfdom.dll location.
 move /Y %INSTALL_DIR%\lib\*.dll %INSTALL_DIR%\bin
+if errorlevel 1 exit 1
