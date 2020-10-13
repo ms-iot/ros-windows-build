857337931af7a716a89661ec7e5acffb53f56e62
seanyen <seanyen@microsoft.com>
2020-10-13 12:41:26 -0700

    Patch rosbridge dependencies.

--- experimental/ros/noetic/patch.bat
+++ experimental/ros/noetic/patch.bat
@@ -3,6 +3,9 @@
 xcopy /Y /S /I .\experimental\ros\%ROS_DISTRO%\pre-patch %INSTALL_DIR%
 set PATH=%INSTALL_DIR%\Scripts;%INSTALL_DIR%;%INSTALL_DIR%\bin;%PATH%
 
+set INCLUDE=C:\opt\ros\noetic\x64\include\python3.8
+set LIB=C:\opt\ros\noetic\x64\Lib
+
 python -m pip config set global.disable-pip-version-check True
 python -m pip install -U pycryptodomex==3.9.8
 python -m pip install -U gnupg==2.3.1
@@ -15,6 +18,16 @@ python -m pip install -U pyassimp==4.1.4
 python -m pip install -U Sphinx==3.2.1
 python -m pip install -U cairocffi==1.1.0
 python -m pip install -U git+https://github.com/ms-iot/rosdep@windows/0.19.0
+
+:: rosbridge related dependencies
+:: https://github.com/RobotWebTools/rosbridge_suite/issues/198
+:: python -m pip install -U bson==0.5.10
+python -m pip install -U pymongo==3.11.0
+python -m pip install -U pyproj==2.6.1.post1
+python -m pip install -U twisted==20.3.0
+python -m pip install -U autobahn==20.7.1
+python -m pip install -U pyOpenSSL==19.1.0
+python -m pip install -U service-identity==18.1.0
 
 :: bootstrap rosdep
 set ROS_ETC_DIR=%INSTALL_DIR%\etc\ros
