8a344eb92b4b6fc6b4c81b2a874a7ef88128e58b
Lou Amadio <lamadio@microsoft.com>
2021-06-22 17:13:30 -0700

    Bootstrap Galactic

---
+++ ros/galactic/build.bat
@@ -0,0 +1,28 @@
+@echo OFF
+
+set ROS_VERSION=2
+set "IGNORED_PACKAGES=rttest test_osrf_testing_tools_cpp tlsf pybind11_vendor joy_linux wiimote spacenav"
+
+colcon build ^
+    --merge-install ^
+    --packages-skip-by-dep %IGNORED_PACKAGES% ^
+    --packages-skip %IGNORED_PACKAGES% ^
+    --install-base %INSTALL_DIR% ^
+    --cmake-target install ^
+    --cmake-args ^
+        -DCMAKE_BUILD_TYPE=RelWithDebInfo ^
+        -DCURL_NO_CURL_CMAKE=ON ^
+        -DBUILD_TESTING:BOOL=False ^
+        -DCMAKE_PROGRAM_PATH=%INSTALL_DIR%\tools\protobuf;%INSTALL_DIR%\tools\qt5\bin ^
+        -DCMAKE_PDB_OUTPUT_DIRECTORY=%PDB_OUTPUT_DIRECTORY% ^
+    2>&1
+if errorlevel 1 exit 1
+
+:: fix srdfdom.dll location.
+move /Y %INSTALL_DIR%\lib\*.dll %INSTALL_DIR%\bin
+if errorlevel 1 exit 1
+
+:: run rosdep check
+set ROS_ETC_DIR=%INSTALL_DIR%\etc\ros
+rosdep check --from-paths "%INSTALL_DIR%\share" --ignore-src 2>&1
+exit 0
---
+++ ros/galactic/repos.ps1
@@ -0,0 +1,46 @@
+[CmdletBinding()]
+param(
+    $badParam
+)
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
+# Powershell2-compatible way of forcing named-parameters
+if ($badParam)
+{
+    throw "Only named parameters are allowed"
+}
+
+try
+{
+    $scriptsDir = split-path -parent $script:MyInvocation.MyCommand.Definition
+    $rootDir = split-path -parent $scriptsDir
+
+    $arguments = @{
+        Distro = "galactic"
+        Version = "galactic/2021-05-23"
+        Packages = @(
+            'navigation2',
+            'desktop',
+            'random_numbers',
+            'joint_state_publisher',
+            'octomap_msgs',
+            'object_recognition_msgs',
+            'turtlebot3_msgs',
+            'gazebo_ros_pkgs',
+            'realtime_tools',
+            'xacro',
+            'eigen_stl_containers',
+            'control_msgs',
+            'urdfdom_py',
+            'moveit_core',
+            'warehouse_ros'
+        )
+    }
+
+    & (Join-Path $rootDir "generateRepos.ps1") @arguments
+}
+catch
+{
+  Write-Warning "Failed to generate galactic repos."
+  throw
+}
