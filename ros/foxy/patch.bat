3a2eb286e67a4774a63b936199ade63e7449301d
Lou Amadio <ooeygui@users.noreply.github.com>
2021-11-04 18:02:53 -0700

    Fixup to enable ogre use on foxy (#205)
    * Fixup to enable ogre use on foxy
    
    * Fix Gazebo break by post patching
    
    * FindOgre is in wrong location
    
    * Gazebo was in the wrong spot

--- ros/foxy/patch.bat
+++ ros/foxy/patch.bat
@@ -1,4 +1,5 @@
 @echo OFF
+set PATH=%INSTALL_DIR%\Scripts;%INSTALL_DIR%;%INSTALL_DIR%\bin;%PATH%
 
 :: remove Vcpkg python38.dll to avoid conflicts
 DEL /F /Q %INSTALL_DIR%\bin\python38.dll
@@ -6,7 +7,7 @@ DEL /F /Q %INSTALL_DIR%\bin\python38.dll
 :: Lock pytest to 6.0.0. 
 :: When running 6.1.0 or later, pytest fails to load due to a dependency on result log.
 :: https://github.com/pytest-dev/pytest-rerunfailures/issues/128
-pip install pytest==6.0.0
+python -m pip install pytest==6.0.0
 
 :: bootstrap vcpkg
 powershell .\ros\%ROS_DISTRO%\vcpkg.ps1 -InstallDir "%INSTALL_DIR%"
