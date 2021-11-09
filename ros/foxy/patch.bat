6c53495892e3ac21a49f0c1306e6f9af14f656d1
Lou Amadio <ooeygui@users.noreply.github.com>
2021-11-09 12:28:04 -0800

    Add rosdep (#207)
    * Add rosdep
    
    * add dsv
    
    * Remove rosdep during build
    
    * Add log output and rosdep init
    
    * Fix stop using post-patch during pre-patch
    
    * update vcpkg

--- ros/foxy/patch.bat
+++ ros/foxy/patch.bat
@@ -4,10 +4,16 @@ set PATH=%INSTALL_DIR%\Scripts;%INSTALL_DIR%;%INSTALL_DIR%\bin;%PATH%
 :: remove Vcpkg python38.dll to avoid conflicts
 DEL /F /Q %INSTALL_DIR%\bin\python38.dll
 
+python -m pip config set global.disable-pip-version-check True
 :: Lock pytest to 6.0.0. 
 :: When running 6.1.0 or later, pytest fails to load due to a dependency on result log.
 :: https://github.com/pytest-dev/pytest-rerunfailures/issues/128
 python -m pip install pytest==6.0.0
+python -m pip install -U git+https://github.com/ms-iot/rosdep@windows/0.20.0
+
+set ROS_ETC_DIR=%INSTALL_DIR%\etc\ros
+rosdep init
+rosdep update
 
 :: bootstrap vcpkg
 powershell .\ros\%ROS_DISTRO%\vcpkg.ps1 -InstallDir "%INSTALL_DIR%"
