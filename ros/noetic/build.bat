deabe48993384d508d6cc88ae8e6a3447fcdab49
Lou Amadio <ooeygui@users.noreply.github.com>
2022-09-27 14:19:28 -0700

    Update build.bat

--- ros/noetic/build.bat
+++ ros/noetic/build.bat
@@ -4,13 +4,6 @@ xcopy /Y /S /I %Build_SourcesDirectory%\ros\noetic\src src
 
 set "IGNORED_PACKAGES=stage stage_ros image_view theora_image_transport rviz_plugin_tutorials four_wheel_steering_controller moveit_resources_prbt_ikfast_manipulator_plugin cartographer_ros_docs"
 
-pip install jinja2<3.1
-pip install sphinx==3.5.4
-
-choco install lua52 --allow-empty-checksums -y
-
-set PATH=C:\ProgramData\chocolatey\lib\lua52\tools;%PATH%
-
 colcon build ^
     --merge-install ^
     --packages-skip-by-dep %IGNORED_PACKAGES% ^
