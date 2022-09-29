b351bff2cb82da4b67ce9cb3cd510ec8013edd13
Lou Amadio <ooeygui@users.noreply.github.com>
2022-09-29 12:35:15 -0700

    Update build.bat

--- ros/foxy/build.bat
+++ ros/foxy/build.bat
@@ -23,7 +23,7 @@ colcon build ^
         -DCURL_NO_CURL_CMAKE=ON ^
         -DBUILD_TESTING:BOOL=False ^
         -DCMAKE_PROGRAM_PATH=%INSTALL_DIR%\tools\protobuf;%INSTALL_DIR%\tools\qt5\bin ^
-        -DCMAKE_PDB_OUTPUT_DIRECTORY=%PDB_OUTPUT_DIRECTORY%
+        -DCMAKE_PDB_OUTPUT_DIRECTORY=%PDB_OUTPUT_DIRECTORY% ^
         -DCMAKE_SYSTEM_VERSION=10.0.19041.0
     2>&1
 if errorlevel 1 exit 1
